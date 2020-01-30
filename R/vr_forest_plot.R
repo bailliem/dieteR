ff2c95b18c048ace4d5d17f9f9ed819a4471461a
bailliem <bailliem@gmail.com>
2020-01-30 21:08:36 +0100

    add to site

--- R/vr_forest_plot.R
+++ R/vr_forest_plot.R
@@ -1,1 +1,22 @@
-##
+#' report_p
+#'
+#' @param p double required
+#' @param digits significant digits
+#'
+#' @return p_string a string
+#' @export
+#'
+#' @examples
+#' report_p(0.02018) # returns "p = .020"
+#' report_p(0.00028) # returns "p < .001"
+report_p <- function(p, digits = 3) {
+  if (p < 0) stop("p cannot be less than 0")
+  if (p > 1) stop("p cannot be greater than 1")
+  if (p < .001) return("p < .001")
+
+  p_round <- round(p, digits)
+  p_string <- paste("p =", p_round)
+
+  return(p_string)
+}
+
--- tests/testthat/test-vr_forest_plot.R
+++ tests/testthat/test-vr_forest_plot.R
@@ -1,3 +1,18 @@
 test_that("multiplication works", {
   expect_equal(2 * 2, 4)
 })
+
+context("report_p")
+
+testthat::test_that("errors", {
+  testthat::expect_error(
+    report_p(-1),
+    "p cannot be less than 0"
+  )
+
+  testthat::expect_error(
+    report_p(2),
+    "p cannot be greater than 1"
+  )
+
+})
